44c9a950ecefe9180e70197924a2d275ba83b7e5
crazykai03 <37287594+crazykai03@users.noreply.github.com>
2023-11-01 09:20:37 +0800

    good

---
+++ newscissorui/main.py
@@ -0,0 +1,454 @@
+# Import module
+from tkinter import *
+from PIL import ImageTk , Image
+import threading
+import time
+lock_press_time =0
+lock_hold= False
+hold_time =0
+hold_status= False
+held_status = False
+lock_state = True
+pre_function_time =0
+rf_tran_data = ""
+command =[ord("B"),ord("E"),ord("2"),ord("1"),0]
+
+
+
+def uppress(event):
+    global pre_function_time
+    pre_function_time = time.time()
+    canvas1.itemconfig(up_btn, image = up_tap)
+
+
+
+def uprelease(event):
+    global pre_function_time
+    pre_function_time = time.time()
+    canvas1.itemconfig(up_btn, image=up)
+    command[4] = 0x01
+
+
+
+def downpress(event):
+        global pre_function_time
+        pre_function_time = time.time()
+        canvas1.itemconfig(down_btn, image=down_tap)
+
+
+def downrelease(event):
+        global pre_function_time
+        pre_function_time = time.time()
+        canvas1.itemconfig(down_btn, image=down)
+        command[4] = 0x02
+
+
+def stoppress(event):
+    global pre_function_time
+    pre_function_time = time.time()
+    canvas1.itemconfig(stop_btn, image = stop_tap)
+    command[4] = 0x03
+
+def stoprelease(event):
+    global pre_function_time
+    pre_function_time = time.time()
+    canvas1.itemconfig(stop_btn, image=stop)
+
+
+
+def lockpress(event):
+    global  button1 ,lock_press_time , lock_hold , lock_state
+    lock_hold = True
+    canvas1.itemconfig(lock_btn, image = lock_tap)
+    lock_state = True
+    lock_press_time= time.time()
+def lockrelease(event):
+    global btn1 , lock_press_time , lock_hold ,lock_state
+    lock_hold = False
+    print(time.time()-lock_press_time)
+    if (lock_state==True):
+        canvas1.itemconfig(lock_btn, image=lock)
+        rgb_pop_up_release()
+        unbind_btn()
+
+
+def light1press(event):
+    global pre_function_time,hold_status,hold_time
+    pre_function_time = time.time()
+    canvas1.itemconfig(light1_btn, image = light1_tap)
+    hold_status = True
+    command[4] = 0x80
+    hold_time = time.time()
+
+
+def light1release(event):
+    global pre_function_time,hold_status,held_status
+    pre_function_time = time.time()
+    canvas1.itemconfig(light1_btn, image=light1)
+    if held_status!=True:
+        command[4] = 0xA8
+    else:
+        command[4] = 0x8A
+    hold_status = False
+    held_status = False
+
+
+def light2press(event):
+    global pre_function_time,hold_status,hold_time
+    pre_function_time = time.time()
+    canvas1.itemconfig(light2_btn, image = light2_tap)
+    hold_status = True
+    command[4] = 0x81
+    hold_time = time.time()
+
+
+def light2release(event):
+    global pre_function_time,hold_status,held_status
+
+    pre_function_time = time.time()
+    canvas1.itemconfig(light2_btn, image=light2)
+    if held_status != True:
+        command[4] = 0xA9
+    else:
+        command[4] = 0x8B
+    hold_status = False
+    held_status = False
+
+def light3press(event):
+    global pre_function_time,hold_status,hold_time
+    pre_function_time = time.time()
+    canvas1.itemconfig(light3_btn, image = light3_tap)
+    hold_status = True
+    command[4] = 0x82
+    hold_time = time.time()
+
+
+def light3release(event):
+    global pre_function_time,hold_status,held_status
+
+    pre_function_time = time.time()
+    canvas1.itemconfig(light3_btn, image=light3)
+    if held_status != True:
+        command[4] = 0xAA
+    else:
+        command[4] = 0x8C
+    hold_status = False
+    held_status = False
+
+def alllightpress(event):
+    global pre_function_time
+    pre_function_time = time.time()
+    canvas1.itemconfig(all_light_btn, image = all_light_tap)
+
+
+def alllightrelease(event):
+    global pre_function_time
+    pre_function_time = time.time()
+    canvas1.itemconfig(all_light_btn, image=all_light)
+    command[4] = 0xD7
+
+def rgbpress(event):
+    global pre_function_time,hold_status,hold_time
+    pre_function_time = time.time()
+    canvas1.itemconfig(rgb1_btn, image = rgb1_tap)
+    hold_status = True
+    command[4] = 0xCB
+    hold_time = time.time()
+
+
+def rgbrelease(event):
+    global pre_function_time,hold_status,held_status
+
+    pre_function_time = time.time()
+    canvas1.itemconfig(rgb1_btn, image=rgb1)
+    if held_status != True:
+        command[4] = 0xC6
+    else:
+        command[4] = 0xD0
+    hold_status = False
+    held_status = False
+
+
+def optionpress(event):
+    global pre_function_time
+    pre_function_time = time.time()
+    canvas1.itemconfig(option_btn, image = option)
+    rgb_pop_up()
+
+
+
+#---------canvas 2
+
+
+def changepress(event):
+    global pre_function_time
+    pre_function_time = time.time()
+    canvas2.itemconfig(change_btn, image = change_tap)
+
+def changerelease(event):
+    global pre_function_time
+    pre_function_time = time.time()
+    canvas2.itemconfig(change_btn, image=change)
+    command[4] = 0xC6
+
+
+def confirmpress(event):
+    global pre_function_time
+    pre_function_time = time.time()
+    canvas2.itemconfig(option_btn, image = confirm_tap)
+
+def confirmrelease(event):
+    global pre_function_time
+    pre_function_time = time.time()
+    canvas2.itemconfig(confirm_btn, image=confirm)
+    rgb_pop_up_release()
+
+
+
+
+
+
+
+
+
+
+
+
+def serial_write():
+    global  command
+    print(command)
+    print(bytes(command))
+
+
+
+
+
+
+
+
+
+
+
+def mylog():
+    global  lock_press_time , lock_state , pre_function_time,held_status
+    if (time.time() - lock_press_time >=2 and lock_hold==True):
+        canvas1.itemconfig(lock_btn, image=unlock)
+        bind_btn()
+        lock_state = False
+        pre_function_time = time.time()
+    elif (time.time() - pre_function_time >=30  and lock_state == False ):
+        canvas1.itemconfig(lock_btn, image=lock)
+        lock_state = True
+        rgb_pop_up_release()
+        unbind_btn()
+
+    if hold_status==True and time.time() - hold_time >=0.5 :
+        held_status = True
+        serial_write()
+    elif  command[4] !=0x00 and hold_status==False:
+        serial_write()
+        command[4]=0x00
+
+
+
+
+    threading.Timer(0.3, mylog).start()
+
+def unbind_btn():
+    canvas1.tag_unbind(up_btn, '<Button-1>' )
+    canvas1.tag_unbind(up_btn, '<ButtonRelease-1>' )
+
+    canvas1.tag_unbind(down_btn, '<Button-1>', )
+    canvas1.tag_unbind(down_btn, '<ButtonRelease-1>' )
+
+    canvas1.tag_unbind(stop_btn, '<Button-1>', )
+    canvas1.tag_unbind(stop_btn, '<ButtonRelease-1>' )
+
+
+    canvas1.tag_unbind(light1_btn, '<Button-1>' )
+    canvas1.tag_unbind(light1_btn, '<ButtonRelease-1>' )
+
+    canvas1.tag_unbind(light2_btn, '<Button-1>' )
+    canvas1.tag_unbind(light2_btn, '<ButtonRelease-1>' )
+
+    canvas1.tag_unbind(light3_btn, '<Button-1>' )
+    canvas1.tag_unbind(light3_btn, '<ButtonRelease-1>')
+
+    canvas1.tag_unbind(all_light_btn, '<Button-1>' )
+    canvas1.tag_unbind(all_light_btn, '<ButtonRelease-1>' )
+
+    canvas1.tag_unbind(rgb1_btn, '<Button-1>' )
+    canvas1.tag_unbind(rgb1_btn, '<ButtonRelease-1>')
+
+    canvas1.tag_unbind(option_btn, '<Button-1>' )
+
+
+
+def bind_btn():
+    canvas1.tag_bind(up_btn, '<Button-1>', uppress)
+    canvas1.tag_bind(up_btn, '<ButtonRelease-1>', uprelease)
+
+    canvas1.tag_bind(down_btn, '<Button-1>', downpress)
+    canvas1.tag_bind(down_btn, '<ButtonRelease-1>', downrelease)
+
+    canvas1.tag_bind(stop_btn, '<Button-1>', stoppress)
+    canvas1.tag_bind(stop_btn, '<ButtonRelease-1>', stoprelease)
+
+    canvas1.tag_bind(lock_btn, '<Button-1>', lockpress)
+    canvas1.tag_bind(lock_btn, '<ButtonRelease-1>', lockrelease)
+
+    canvas1.tag_bind(light1_btn, '<Button-1>', light1press)
+    canvas1.tag_bind(light1_btn, '<ButtonRelease-1>', light1release)
+
+    canvas1.tag_bind(light2_btn, '<Button-1>', light2press)
+    canvas1.tag_bind(light2_btn, '<ButtonRelease-1>', light2release)
+
+    canvas1.tag_bind(light3_btn, '<Button-1>', light3press)
+    canvas1.tag_bind(light3_btn, '<ButtonRelease-1>', light3release)
+
+    canvas1.tag_bind(all_light_btn, '<Button-1>', alllightpress)
+    canvas1.tag_bind(all_light_btn, '<ButtonRelease-1>', alllightrelease)
+
+    canvas1.tag_bind(rgb1_btn, '<Button-1>', rgbpress)
+    canvas1.tag_bind(rgb1_btn, '<ButtonRelease-1>', rgbrelease)
+
+    canvas1.tag_bind(option_btn, '<Button-1>', optionpress)
+
+
+def rgb_pop_up():
+    canvas1.pack_forget()
+    canvas2.pack()
+    canvas2.tag_bind(change_btn, '<Button-1>', changepress)
+    canvas2.tag_bind(change_btn, '<ButtonRelease-1>', changerelease)
+    canvas2.tag_bind(confirm_btn, '<Button-1>', confirmpress)
+    canvas2.tag_bind(confirm_btn, '<ButtonRelease-1>', confirmrelease)
+
+def rgb_pop_up_release():
+    canvas2.pack_forget()
+    canvas1.pack()
+    canvas2.tag_unbind(change_btn, '<Button-1>' )
+    canvas2.tag_unbind(change_btn, '<ButtonRelease-1>' )
+    canvas2.tag_unbind(confirm_btn, '<Button-1>' )
+    canvas2.tag_unbind(confirm_btn, '<ButtonRelease-1>' )
+
+
+
+# Create object
+root = Tk()
+
+# Adjust size
+root.geometry("800x480")
+
+# Add image file
+bg = PhotoImage(file="Bg.png")
+up= PhotoImage(file="up.png")
+down= PhotoImage(file="down.png")
+stop = PhotoImage(file="stop.png")
+light1= PhotoImage(file="light1.png")
+light2= PhotoImage(file="light2.png")
+light3= PhotoImage(file="light3.png")
+rgb1= PhotoImage(file="rgb.png")
+all_light = PhotoImage(file="alllight.png")
+pop_up_bg = PhotoImage(file="Popup-Bg.png")
+change =  PhotoImage(file="change_btn.png")
+confirm =  PhotoImage(file="confirm_btn.png")
+lock= PhotoImage(file="lock.png")
+unlock = PhotoImage(file="unlock.png")
+option = PhotoImage(file="option.png")
+#------------for tap button--------------------------------
+
+up_tap= PhotoImage(file="up_tap.png")
+down_tap= PhotoImage(file="down_tap.png")
+stop_tap = PhotoImage(file="stop_tap.png")
+light1_tap= PhotoImage(file="light1_tap.png")
+light2_tap= PhotoImage(file="light2_tap.png")
+light3_tap= PhotoImage(file="light3_tap.png")
+rgb1_tap= PhotoImage(file="rgb_tap.png")
+all_light_tap = PhotoImage(file="all_tap.png")
+lock_tap= PhotoImage(file="lock_tap.png")
+unlock_tap= PhotoImage(file = "unlock_tap.png")
+change_tap =  PhotoImage(file="change_tap.png")
+confirm_tap =  PhotoImage(file="confirm_tap.png")
+all_dark = PhotoImage(file = "all_dark_bg.png")
+
+
+
+
+
+
+
+# Create Canvas
+canvas1 = Canvas(root, width=800,
+                 height=480,bd=0, highlightthickness=0 , bg="#2B2E35")
+canvas2 = Canvas(root, width=800,
+                 height=480,bd=0, highlightthickness=0 , bg="#2B2E35")
+canvas1.pack(fill="both", expand=True)
+
+
+# Display image
+#canvas1.create_image(0, 0, image=bg,anchor="nw")
+
+
+
+
+
+
+offset_x = 72
+offset_y = 108
+
+
+
+# Display Buttons
+bg_img_all = canvas1.create_image(400,240,image=bg)
+
+up_btn = canvas1.create_image(16+offset_x,16+offset_y,image=up)
+down_btn = canvas1.create_image(16+offset_x,248+offset_y,image=down)
+stop_btn =  canvas1.create_image(640+offset_x,16+offset_y,image=stop)
+lock_btn =  canvas1.create_image(640+offset_x,248+offset_y,image=lock)
+
+light1_btn =  canvas1.create_image(176+offset_y,16+offset_y,image=light1)
+light2_btn =  canvas1.create_image(176+offset_y,248+offset_y,image=light2)
+light3_btn =  canvas1.create_image(408+offset_y,16+offset_y,image=light3)
+rgb1_btn =  canvas1.create_image(408+offset_y,248+offset_y,image=rgb1)
+all_light_btn =  canvas1.create_image(280+120,120+120,image=all_light)
+option_btn =  canvas1.create_image(480+offset_y,185+offset_y,image=option)
+
+
+
+
+#------canvas 2 -----------------
+bg_img_dark = canvas2.create_image(400,240,image=all_dark)
+pop_up_background =  canvas2.create_image(176+224,16+224,image=pop_up_bg)
+change_btn =  canvas2.create_image(400,220,image=change)
+confirm_btn =  canvas2.create_image(400,220+170,image=confirm)
+
+
+
+
+bind_btn()
+unbind_btn()
+
+
+
+
+
+
+
+
+
+
+#canvas1.itemconfig(lock,state='hidden')
+
+
+
+
+
+
+mylog()
+
+
+# Execute tkinter
+
+root.overrideredirect(True)
+
+root.mainloop()
